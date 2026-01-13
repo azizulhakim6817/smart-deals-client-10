fbad9bc42c1826c072e9ea02eba7639d135f223d
azizul6817 <azizulhakim68178@gmail.com>
2026-01-13 20:15:37 +0600

    add

--- src/constext/AuthProvider.jsx
+++ src/constext/AuthProvider.jsx
@@ -18,24 +18,21 @@ const AuthProvider = ({ children }) => {
 
   //!Register----------
   const createUser = (email, password) => {
-    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
   //!sign-in/login---------
   const signInUser = (email, password) => {
-    setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
+  
   };
 
   //!signOut -----------
   const signOutUser = () => {
-    setLoading(true);
     return signOut(auth);
   };
 
   //!google sing-in-------------
   const googleSignIn = () => {
-    setLoading(true);
     return signInWithPopup(auth, gooleProvider);
   };
 
